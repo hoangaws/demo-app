740062a3866c1e7d4029bc20a587a654a346edea
DESKTOP-UMJT64L\HoangPC <huyhoang888@gmail.com>
2018-04-08 04:30:04 +0700

    init app

---
+++ components/samples/index.js
@@ -0,0 +1,11 @@
+import ExpoConfigView from './ExpoConfigView';
+import ReadingLinksView from './ReadingLinksView';
+import ListeningLinksView from './ListeningLinksView';
+import VocabularyLinksView from './VocabularyLinksView';
+
+module.exports = {
+  ExpoConfigView,
+  ReadingLinksView,
+  ListeningLinksView,
+  VocabularyLinksView,
+};
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+
+import Colors from '../constants/Colors';
+
+import VocabularyScreen from '../screens/VocabularyScreen';
+import ReadingScreen from '../screens/ReadingScreen';
+import ListeningScreen from '../screens/ListeningScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+export default TabNavigator(
+  {
+    Vocabulary: {
+      screen: VocabularyScreen,
+    },
+    Reading: {
+      screen: ReadingScreen,
+    },
+    Listening: {
+      screen: ListeningScreen,
+    },
+    Settings: {
+      screen: SettingsScreen,
+    },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Vocabulary':
+            iconName =
+              Platform.OS === 'ios'
+                ? `ios-information-circle${focused ? '' : '-outline'}`
+                : 'md-information-circle';
+            break;
+          case 'Reading':
+            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
+            break;
+          case 'Listening':
+            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
+            break;
+          case 'Settings':
+            iconName =
+              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3, width: 25 }}
+            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+          />
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
